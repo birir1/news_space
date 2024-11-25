b33915c1c75a819c70a86caf28a19a0840286960
birir1 <sospeterbirir1@gmail.com>
2024-11-25 17:20:28 +0300

    version 1.0.2

---
+++ resources/views/frontend/technology.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Americas News | Page')
+
+@section('content')
+
+    @include("frontend.partials.navbar1")
+
+    @include("frontend.partials.navbar2")
+
+    @include("frontend.partials.landing")
+    
+@endsection
